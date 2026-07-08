dc40b20f8e9195f06b7c39fc71efe48dcca298a4
Angelo Arcillas <angeloarcillas64@gmail.com>
2026-07-08 22:47:49 +0800

    style: add top padding to main layout container to prevent header overlap

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ export const Layout: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col bg-background text-foreground">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 pt-24">
         <Outlet />
       </main>
       <Footer />
